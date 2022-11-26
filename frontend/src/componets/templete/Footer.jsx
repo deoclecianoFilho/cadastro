a5ccc6c12b5928ee519b30437ce612df8d079214
DeoclecianoCS <deocleciano4@gmail.com>
2022-11-26 15:38:10 -0300

    Criando componete Rodape

--- frontend/src/componets/templete/Footer.jsx
+++ frontend/src/componets/templete/Footer.jsx
@@ -3,5 +3,8 @@ import './Footer.css'
 
 export default props =>
     <footer className="footer">
-        Rodape
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Deocleciano<span className="text-danger">C</span>S</strong>
+        </span>
     </footer>
