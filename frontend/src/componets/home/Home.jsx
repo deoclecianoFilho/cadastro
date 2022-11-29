77d670e103eab347fa4d2a2165cb27c853952149
DeoclecianoCS <deocleciano4@gmail.com>
2022-11-28 21:32:01 -0300

    Componente Cadastro Usuario

--- frontend/src/componets/home/Home.jsx
+++ frontend/src/componets/home/Home.jsx
@@ -4,7 +4,7 @@ import Main from "../templete/Main";
 
 export default props =>
 
-<Main icon="home" title="Inicio" subtitle="Sugundo Projeto do capituo de React">
+<Main icon="home" title="Inicio" subtitle="Segundo Projeto do capituo de React">
     <div className="display-4">Bem Vindo!</div>
     <hr />
     <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
